6dc3b6bd35551c87b32c764d574509fe0b0e90be
Akkamahadevimangane <akkamahadevi.akkamahadevi@ibm.com>
2025-12-19 09:56:44 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity1.java
@@ -0,0 +1,23 @@
+package examples;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity1 {
+	public static void main(String[] args)
+	{
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://training-support.net");
+		 
+		System.out.println("Page title is: " + driver.getTitle());
+	        // Find the About Us link and click it
+	        driver.findElement(By.linkText("About Us")).click();
+	        
+	        // Print the page title of the About Us page
+	     System.out.println("New page title is: " + driver.getTitle());
+		
+		driver.quit();
+		
+}
+}
