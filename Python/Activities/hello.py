46c8cb2912d034a6e5f8e5cb89f7dbf6e4430b9d
Akkamahadevimangane <akkamahadevi.akkamahadevi@ibm.com>
2025-12-19 22:17:01 +0530

    Add files via upload

---
+++ Python/Activities/hello.py
@@ -0,0 +1,22 @@
+# comment line
+''' Comment multiple lines Thats it 
+Many line '''
+
+# Just print it.
+print("Hello, World!", end="")
+
+# assigne and print it.
+test = "Hello,World"
+print(test.split(","), end="")
+
+# assigne, type full.
+D = str("Devi")
+print(D)
+
+a = 3
+b = 10
+
+if a > b:
+print("b is greatest")
+else:
+print ("a is greatest")
